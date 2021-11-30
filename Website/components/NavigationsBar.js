4cba10aff528bb5e783de2618dbf791fd592cbe4
jeff0167 <73894404+jeff0167@users.noreply.github.com>
2021-11-30 14:52:49 +0100

    opret bruger og component

---
+++ Website/components/NavigationsBar.js
@@ -0,0 +1,20 @@
+app.component('navigations-bar', {
+
+    template: 
+    /*html*/
+    `   <div id="navdiv">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
+        <a class="navbar-brand" href="#" id="logoButton"><img src="Images/hus.png" alt="logo" class="rounded-circle"></a>
+        <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="./index.html" id="homeButton">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="katalog.html" id="katalogButton">Katalog</a>
+            <a class="nav-item nav-link" href="#">Placeholder</a>
+            <a class="nav-item nav-link" href="./login.html">Profil</a>
+        </div>
+        <div class="justify-content-end">
+            <a class="nav-item nav-link border border-dark" href="./login.html" id="loginButton">Login</a>
+            <a class="nav-item nav-link" href="./createUser.html" id="signUpButton">Sign up</a>
+        </div>
+    </nav>
+</div>`,
+  })
